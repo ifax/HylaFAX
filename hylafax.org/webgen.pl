26e8c15d6b8a96c915e325a6c4bb448bf2f950d0
Darren Nickerson <darren.nickerson@ifax.com>
1998-10-28 01:50:11 +0000

    *** empty log message ***

--- hylafax.org/webgen.pl
+++ hylafax.org/webgen.pl
@@ -2,31 +2,4 @@
 
 use strict;
 
-#open(CONFIG,"|../configure --quiet");
-#close CONFIG;a
-`../configure
-	--quiet
-	--with-DIR_BIN=/usr/bin
-	--with-DIR_SBIN=/usr/sbin
-	--with-DIR_LIBEXEC=/usr/sbin
-	--with-DIR_LIBDATA=/usr/share/fax
-	--with-DIR_LOCKS=/var/lock
-	--with-TIFFINC=/usr/include
-	--with-TIFFBIN=/usr/bin
-	--with-DIR_MAN=/usr/man
-	--with-PATH_GSRIP=/usr/bin/gs
-	--with-DEFVRES=196
-	--with-DBLIBINC=/usr/include
-	--with-DIR_AFM=/usr/share/fax/afm
-	--with-LIBTIFF=-ltiff
-	--with-DIR_HTML=/tmp
-	--with-DIR_CGI=/usr/doc
-	--with-DIR_SPOOL=/var/spool/fax
-	--with-LIBDB=-ldb
-	--with-ZLIB=no
-	--with-ZLIBINC=.
-	--with-AFM=no
-	--with-DSO=LINUX
-	--with-DSOSUF=so
-	--with-PATH_VGETTY=/sbin/vgetty
-	--with-HTML=yes`
+`../configure --quiet --with-DIR_BIN=/usr/bin --with-DIR_SBIN=/usr/sbin --with-DIR_LIBEXEC=/usr/sbin --with-DIR_LIBDATA=/usr/share/fax --with-DIR_LOCKS=/var/lock --with-TIFFINC=/usr/include --with-TIFFBIN=/usr/bin --with-DIR_MAN=/usr/man --with-PATH_GSRIP=/usr/bin/gs --with-DEFVRES=196 --with-DBLIBINC=/usr/include --with-DIR_AFM=/usr/share/fax/afm --with-LIBTIFF=-ltiff --with-DIR_HTML=/tmp --with-DIR_CGI=/usr/doc --with-DIR_SPOOL=/var/spool/fax --with-LIBDB=-ldb --with-ZLIB=no --with-ZLIBINC=. --with-AFM=no --with-DSO=LINUX --with-DSOSUF=so --with-PATH_VGETTY=/sbin/vgetty --with-HTML=yes`
