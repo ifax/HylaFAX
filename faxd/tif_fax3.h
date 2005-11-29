38c7b404aa5dd4b243678029c442b02ac8b282d1
Lee Howard <faxguy@howardsilvan.com>
2005-11-29 21:23:56 +0000

    Bug 708: prevent hangs in Phase C when the modem or the sender may get into          a funk that causes an infinite loop in the decoder.

--- faxd/tif_fax3.h
+++ faxd/tif_fax3.h
@@ -255,21 +255,27 @@ static const char* StateNames[] = {
  * 11 consecutive zero bits.  This means that if EOLcnt
  * is non-zero then we still need to scan for the final flag
  * bit that is part of the EOL code.
+ *
+ * In order to prevent getting forever stuck in these loops
+ * (perhaps by a hung modem or hung sender) we raiseRTC if
+ * it appears to be the case.
  */
 #define	SYNC_EOL(eoflab) do {						\
     if (EOLcnt == 0) {							\
-	for (;;) {							\
+	for (u_long i = 0;; i++) {					\
 	    NeedBits16(11,eoflab);					\
 	    if (GetBits(11) == 0)					\
 		break;							\
 	    ClrBits(1);							\
+	    if (i > 150000) raiseRTC();					\
 	}								\
     }									\
-    for (;;) {								\
+    for (u_long i = 0;; i++) {						\
 	NeedBits8(8,eoflab);						\
 	if (GetBits(8))							\
 	    break;							\
 	ClrBits(8);							\
+	if (i > 150000) raiseRTC();					\
     }									\
     while (GetBits(1) == 0)						\
 	ClrBits(1);							\
